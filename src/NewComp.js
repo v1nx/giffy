3b161531b2a9771ce76462d10eef35969f61938e
v1nx <pg18037@ues.edu.sv>
2021-07-09 11:04:02 -0600

    Este es el segundo y a ver si se puede subir

---
+++ src/NewComp.js
@@ -0,0 +1,16 @@
+function NewComp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a>I really think this is a great app
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default NewComp;
